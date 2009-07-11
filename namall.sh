176ce710cda35e5407def62735d3b7097761a7ef
Arkham <liuju86@gmail.com>
2009-07-11 02:08:49 +0200

    namall: Small fixes
    Signed-off-by: Arkham <liuju86@gmail.com>

--- namall.sh
+++ namall.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
+NC='\e[0m'
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+YELLOW='\e[1;30m'
+
 for x in $(find . -maxdepth 1 -type d | sort); do
     cd "$HOME/builds/$x"
 
     if [ -f PKGBUILD ]; then
-        echo "*** $x"
-        echo "==> namcap PKGBUILD..."
-        namcap PKGBUILD || exit 1
+        echo -e "${YELLOW}*** $x${NC}"
+        echo -e "${GREEN}==> ${NC}namcap PKGBUILD..."
+        namcap PKGBUILD | grep -v 'CVS Id'
 
         for pkg in $(find . -name "*pkg.tar.gz"); do
-            echo "==> namcap $(basename $pkg)"
-            namcap $pkg || exit 1
+            echo -e "${GREEN}==> ${NC}namcap $(basename $pkg)"
+            namcap $pkg
         done
 
         echo ""
